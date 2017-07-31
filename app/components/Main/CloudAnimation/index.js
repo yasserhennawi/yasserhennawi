feb143aa13b21fec73ebce30a34a61208b65626f
Yasser Hennawi <yahennawi@gmail.com>
2017-07-31 02:26:26 +0200

    Fixing Cloud animation.

--- app/components/Main/CloudAnimation/index.js
+++ app/components/Main/CloudAnimation/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import styled , { keyframes } from 'utils/styled-components';
-import CloudComponent from 'components/icons/Cloud';
-import CloudSVG from 'components/icons/Cloud/vector.svg';
+import styled, { keyframes } from 'utils/styled-components';
+import media from 'utils/media';
+import FirstCloudSVG from 'components/icons/Cloud/firstCloud.svg';
+import SecondCloudSVG from 'components/icons/Cloud/secondCloud.svg';
+import ThirdCloudSVG from 'components/icons/Cloud/thirdCloud.svg';
+import ForthCloudSVG from 'components/icons/Cloud/forthCloud.svg';
 
-const positionTransition1 = keyframes`
-  0% {background-position: -10%  20%; }
-  100% {background-position: 120%  20%; }
-`
-const positionTransition2 = keyframes`
-  0% {background-position: -30%  70%; }
-  100% {background-position: 140%  70%; }
+const row1Duration = '10s';
+const row2Duration = '14s';
+const row3Duration = '14s';
+const row4Duration = '16s';
+
+const transition = keyframes`
+  0% {background-position-x: 0}
+  100% {background-position-x: 200%}
 `
 const Wrapper = styled.div`
   position: fixed;
@@ -19,32 +23,52 @@ const Wrapper = styled.div`
   background-position: center;
   background-size: cover;
   background-color: #00bffd;
-  opacity: 0.1;
-  ${'' /* animation: ${bgTransition} 2s infinite; */}
+  opacity: 0.15;
   animation-timing-function: linear;
+  ${media.tablet`
+    & * {
+      display: none;
+    }
+  `}
 `;
 const Cloud = styled.div`
-  background: url(${CloudSVG});
+  animation: ${transition} 2s linear infinite;
   position: absolute;
   width: 100%;
   height: 100vh;
   background-repeat: no-repeat;
 `
-const Cloud1 = styled(Cloud)`
-  animation: ${positionTransition1} 9s infinite;
-  animation-timing-function: linear;
-  background-size: 10% ;
+const CloudRow1 = styled(Cloud)`
+  background: url(${FirstCloudSVG}) repeat-x;
+  background-position-y: 90%;
+  background-size: 66.6%;
+  animation-duration: ${row1Duration};
 `
-const Cloud2 = styled(Cloud)`
-  animation: ${positionTransition2} 5s infinite;
-  animation-timing-function: linear;
-  background-size: 20% ;
+const CloudRow2 = styled(Cloud)`
+  background: url(${SecondCloudSVG}) repeat-x;
+  background-position-y: 45%;
+  background-size: 40%;
+  animation-duration: ${row2Duration};
+`
+const CloudRow3 = styled(Cloud)`
+  background: url(${ThirdCloudSVG}) repeat-x;
+  background-position-y: 20%;
+  background-size: 28.5%;
+  animation-duration: ${row3Duration};
+`
+const CloudRow4 = styled(Cloud)`
+  background: url(${ForthCloudSVG}) repeat-x;
+  background-position-y: 5%;
+  background-size: 20%;
+  animation-duration: ${row4Duration};
 `
 
 const CloudAnimation = ({ ...props }) => (
   <Wrapper>
-    <Cloud1 />
-    <Cloud2 />
+    <CloudRow1/>
+    <CloudRow2/>
+    <CloudRow3/>
+    <CloudRow4/>
   </Wrapper>
 )
 
